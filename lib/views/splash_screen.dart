16b7f31f0619f61c64270001f78667f4b0ab7969
Sasha <34662549+alex2704@users.noreply.github.com>
2021-12-02 02:39:13 +0300

    Сверстал SplashScreen;

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -25,8 +25,27 @@ class SplashScreen extends StatelessWidget {
             child: BlocBuilder<SplashScreenBloc, SplashScreenState>(
               builder: (context, state) {
                 if ((state is InitialSplashScreenState) || (state is LoadingSplashScreenState)) {
-                  return const Center(
-                    child: Text('ЗАГРУЗКА')
+                  return Column(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Expanded(
+                        flex: 5,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text('THOUGHTS', style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            letterSpacing: 15,
+                            fontWeight: FontWeight.w600
+                          ),),
+                        ),
+                      ),
+                      Expanded(
+                          flex: 5,
+                      child:Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Image.asset('assets/images/splash.png'))),
+                    ]
                   );
                 } else if (state is LoadedSplashScreenState) {
                   return const FeedScreen();
