1e2b573ec72898ba992706c1434da892ed04e603
Sasha <34662549+alex2704@users.noreply.github.com>
2021-12-06 17:56:09 +0300

    Удалил комментарии;

--- lib/services/api/post_api_provider.dart
+++ lib/services/api/post_api_provider.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:thoughts/entities/post.dart';
-import 'package:http/http.dart' as http;
 import 'dart:developer' as developer;
 
 class PostProvider {
@@ -31,46 +28,4 @@ class PostProvider {
 
     return _posts;
   }
-  // final CollectionReference postList = FirebaseFirestore.instance.collection("posts");
-
-
-
-  // Future<List<Post>> getAllPosts() async {
-  //   final response = await http.get(Uri.parse(''));
-  //
-  //   if (response.statusCode == 200) {
-  //     final List<dynamic> postJson = json.decode(response.body);
-  //     return postJson.map((json) => Post.fromJson(json)).toList();
-  //   } else {
-  //     throw Exception('Error fetching post');
-  //   }
-  // }
-  
-  // Future getPostList() async {
-  //
-  //   List itemsList = [];
-  //   try {
-  //     await postList.get().then((querySnapshot) {
-  //       for (var element in querySnapshot.docs) {
-  //         itemsList.add(element.data());
-  //       }
-  //     });
-  //   } catch (e) {
-  //     throw Exception('Error fetching post');
-  //   }
-  // }
-  //
-  // Future<List<Post>> getAllPosts() async {
-  //   List<Post> itemsList = [];
-  //   try {
-  //     await postList.get().then((querySnapshot) {
-  //       for (var element in querySnapshot.docs) {
-  //         itemsList.add(element.data());
-  //       }
-  //     });
-  //     return itemsList.map((json) => Post.fromJson(json)).toList();
-  //   } catch (e) {
-  //     throw Exception('Error fetching post');
-  //   }
-  // }
 }
