54bd454566b6a71a64df745f3f5e7c31b8bf31f3
Sasha <34662549+alex2704@users.noreply.github.com>
2021-12-15 03:59:28 +0300

    Переход на пост только при клике на текст;

--- lib/views/components/feed_item.dart
+++ lib/views/components/feed_item.dart
@@ -13,89 +13,90 @@ class FeedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        child: Column(children: [
-          Container(
-            // color: Colors.orange,
-            child: IntrinsicHeight(
-                child: Row(
+    return Column(children: [
+      Container(
+        // color: Colors.orange,
+        child: IntrinsicHeight(
+            child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    post.infoUser.idAvatar == 0
-                        ? const CircleAvatar(
-                            backgroundImage:
-                                AssetImage('assets/images/no_photo.jpg'),
-                            radius: 30,
-                          )
-                        : Container(),
-                    const Icon(
-                      Icons.favorite,
-                      color: Colors.redAccent,
-                      size: 30.0,
-                    )
-                  ],
-                ),
-                const SizedBox(width: 15),
-                Expanded(
-                  child: Column(
+                post.infoUser.idAvatar == 0
+                    ? const CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/images/no_photo.jpg'),
+                        radius: 30,
+                      )
+                    : Container(),
+                const Icon(
+                  Icons.favorite,
+                  color: Colors.redAccent,
+                  size: 30.0,
+                )
+              ],
+            ),
+            const SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(children: [
-                            Text(
-                              post.infoUser.name,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10.0,
-                            ),
-                            Text(
-                              '@${post.infoUser.login}',
-                              style: const TextStyle(
-                                  color: CustomColors.light_grey),
-                            )
-                          ]),
-                          const Icon(Icons.more_horiz,
-                              color: Colors.black, size: 30.0)
-                        ],
-                      ),
-                      Text(
+                      Row(children: [
+                        Text(
+                          post.infoUser.name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          '@${post.infoUser.login}',
+                          style:
+                              const TextStyle(color: CustomColors.light_grey),
+                        )
+                      ]),
+                      const Icon(Icons.more_horiz,
+                          color: Colors.black, size: 30.0)
+                    ],
+                  ),
+                  InkWell(
+                      child: Text(
                         '${post.description}',
                         textAlign: TextAlign.left,
                       ),
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const FaIcon(
-                              FontAwesomeIcons.comment,
-                              size: 25.0,
-                            ),
-                            Text(
-                              post.dateCreated,
-                              style: const TextStyle(color: CustomColors.light_grey),
-                            )
-                          ],
+                      onTap: () => _navigateToPostPage(context, post)),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const FaIcon(
+                          FontAwesomeIcons.comment,
+                          size: 25.0,
                         ),
-                        margin: const EdgeInsets.only(top: 20),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            )),
-            padding: const EdgeInsets.only(
-                top: 20.0, left: 20.0, bottom: 20.0, right: 20.0),
-          ),
-          const Divider(color: CustomColors.light_grey, thickness: 1)
-        ]),
-        onTap: () => _navigateToPostPage(context, post));
+                        Text(
+                          post.dateCreated,
+                          style:
+                              const TextStyle(color: CustomColors.light_grey),
+                        )
+                      ],
+                    ),
+                    margin: const EdgeInsets.only(top: 20),
+                  )
+                ],
+              ),
+            ),
+          ],
+        )),
+        padding: const EdgeInsets.only(
+            top: 20.0, left: 20.0, bottom: 20.0, right: 20.0),
+      ),
+      const Divider(color: CustomColors.light_grey, thickness: 1)
+    ]);
   }
 
   void _navigateToPostPage(BuildContext context, post) {
